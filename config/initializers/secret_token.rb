30beb697799c9664fd5859e5b897ed43d4fc3955
ChanceDoor <cd19900419@hotmail.com>
2013-04-10 15:43:28 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '7e19268a8790f3c93c5b7a651ddc18d99e4f321b113c8b0d697ab6fc65a86077f9e677b6f4dab952113b4a23a5209ea2e89b52d8b18e5f34404bd03bd2e58d02'
